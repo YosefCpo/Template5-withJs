0f81c68c5f70e2be0fc10379208786d768b89ef9
YosefCpo <cpo.yosef@yahoo.com>
2022-01-23 11:17:04 +0200

    Backgourd Images Option

--- js/main.js
+++ js/main.js
@@ -39,19 +39,77 @@ colorsLi.forEach((li) => {
 	});
 });
 
+// Switch Random Background Options
+let radndomBackEl = document.querySelectorAll(".random-backgrounds span");
+
+// Random Background option
+let backgroundOption = true;
+
+let theBackgroundInterval;
+
+// Loop on all Spans
+radndomBackEl.forEach((el) => {
+	el.addEventListener("click", (e) => {
+		// Active Class
+		radndomBackEl.forEach((el) => {
+			el.classList.remove("active");
+		});
+		el.classList.add("active");
+
+		if (e.target.dataset.background === "yes") {
+			backgroundOption = true;
+			randomizeImage();
+		} else {
+			backgroundOption = false;
+			clearInterval(theBackgroundInterval);
+			document.querySelector(".landing-page").style.backgroundImage =
+				"url('Images/1.png')";
+		}
+
+		// Local Storage
+		if (e.target.dataset.background === "yes") {
+			window.localStorage.setItem("background_option", "yes");
+		} else {
+			window.localStorage.setItem("background_option", "no");
+		}
+	});
+});
+
+// Get From Local storage
+if (window.localStorage.getItem("background_option") !== null) {
+	clearInterval(theBackgroundInterval);
+	if (window.localStorage.getItem("background_option") === "yes") {
+		backgroundOption = true;
+		randomizeImage();
+	} else {
+		backgroundOption = false;
+		clearInterval(theBackgroundInterval);
+		document.querySelector(".landing-page").style.backgroundImage =
+			"url('images/1.png')";
+	}
+}
+
+radndomBackEl.forEach((span) => {
+	span.classList.remove("active");
+	if (
+		span.dataset.background === window.localStorage.getItem("background_option")
+	) {
+		span.classList.add("active");
+	}
+});
+
 // Select Landing Page Element
 let landingPage = document.querySelector(".landing-page");
 
 // Get Array Of Images
 let imagesArr = ["1.png", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
 
-// Change Background Image Url
-landingPage.style.backgroundImage = `url("images/3.jpg")`;
+function randomizeImage() {
+	if (backgroundOption === true) {
+		theBackgroundInterval = setInterval(() => {
+			let randNum = Math.floor(Math.random() * imagesArr.length);
 
-// Get Random Number
-
-setInterval(() => {
-	let randNum = Math.floor(Math.random() * imagesArr.length);
-
-	landingPage.style.backgroundImage = `url("images/${imagesArr[randNum]}")`;
-}, 10000);
+			landingPage.style.backgroundImage = `url("images/${imagesArr[randNum]}")`;
+		}, 1000);
+	}
+}
